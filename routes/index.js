b930649bd2954c2d570ea65e05c8f5b26022817c
Alper <31041738+4ppe@users.noreply.github.com>
2019-08-10 07:09:00 +0300

    configFactory

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Live Balls' });
+});
+
+router.get('/getEnv', function(req, res, next) {
+  const envData = require('../config/env.json')[process.env.NODE_ENV || 'development'];
+  res.json(envData);
 });
 
 module.exports = router;
