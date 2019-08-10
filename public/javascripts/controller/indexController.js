ae0542b77f2ce896e328ed4762c17d7887728414
Alper <31041738+4ppe@users.noreply.github.com>
2019-08-10 05:56:39 +0300

    bug fix

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -36,105 +36,102 @@ app.controller('indexController',['$scope','indexFactory', ($scope, indexFactory
 
     }
 
-    function initSocket(username) {
+    async function initSocket(username) {
         const connectionOptions = {
             reconnectionAttempts: 3,
             reconnectionDelay: 600
         };
 
-        indexFactory.connectSocket('http://localhost:3000',connectionOptions)
-            .then((socket) => { 
-                socket.emit('newUser', { username });
+        try{
+            const socket =  await indexFactory.connectSocket('http://localhost:3000',connectionOptions);
+            socket.emit('newUser', { username });
 
-                socket.on('initPlayers',(players) => {
-                    $scope.players = players;
-                    $scope.$apply();
-                })
+            socket.on('initPlayers',(players) => {
+                $scope.players = players;
+                $scope.$apply();
+            });
 
-                socket.on('newUser',(data)=> {
-                    const messageData = {
-                        type: {
-                            code: 0, // server(0) or user message(1)
-                            message: 1 // disconnect message(0) or login(1)
-                        },
-                        username: data.username
-                    };
+            socket.on('newUser',(data)=> {
+                const messageData = {
+                    type: {
+                        code: 0, // server(0) or user message(1)
+                        message: 1 // disconnect message(0) or login(1)
+                    },
+                    username: data.username
+                };
 
-                    $scope.messages.push(messageData);
-                    $scope.players[data.id] = data ;
-                    scrollTop();
-                    $scope.$apply();
+                $scope.messages.push(messageData);
+                $scope.players[data.id] = data ;
+                scrollTop();
+                $scope.$apply();
+            });
+
+            socket.on('disUser', (user) => {
+                const messageData = {
+                    type: {
+                        code: 0,
+                        message: 0
+                    }, // info
+                    username: user.username
+                };
+                $scope.messages.push(messageData);
+                delete $scope.players[user.id];
+                scrollTop();
+                $scope.$apply();   
+            });
+
+            socket.on('animate',data => {
+                $('#'+ data.socketId).animate({ 'left': data.x, 'top': data.y }, () => {
+                    animate = false;
                 });
+            });
 
-                socket.on('disUser', (user) => {
-                    const messageData = {
-                        type: {
-                            code: 0,
-                            message: 0
-                        }, // info
-                        username: user.username
-                    };
-                    $scope.messages.push(messageData);
-                    delete $scope.players[user.id];
-                    scrollTop();
-                    $scope.$apply();   
-                });
+            socket.on('newMessage', message => {
+                $scope.messages.push(message);
+                $scope.$apply();
+                showBubble(message.socketId, message.text);
+                scrollTop();
+            });
+                
+            let animate = false; // to wait for the next animation
+            $scope.onClickPlayer = ($event) => {
+                if (!animate){
+                    let x = $event.offsetX;
+                    let y = $event.offsetY;
 
-                socket.on('animate',data => {
-                    $('#'+ data.socketId).animate({ 'left': data.x, 'top': data.y }, () => {
+                    socket.emit('animate',{x, y});
+
+                    animate = true;
+                    $('#'+ socket.id).animate({ 'left': x, 'top': y }, () =>{
                         animate = false;
                     });
-                });
+                }
+            };
 
-                socket.on('newMessage', message => {
-                    $scope.messages.push(message);
-                    $scope.$apply();
-                    showBubble(message.socketId, message.text);
+            $scope.newMessage = () => {
+                let message = $scope.message;
+                const messageData = {
+                    type: {
+                        code:1
+                    }, 
+                    username,
+                    text: message
+                };  
+
+                if( messageData.text && messageData.text.length > 0 && messageData.text.length < 100 ) {
+
+                    $scope.messages.push(messageData); 
+                    socket.emit('newMessage', messageData);
+                    showBubble(socket.id, message);
                     scrollTop();
-                });
-                 
-                let animate = false; // to wait for the next animation
-                $scope.onClickPlayer = ($event) => {
-                    if (!animate){
-                        let x = $event.offsetX;
-                        let y = $event.offsetY;
 
-                        socket.emit('animate',{x, y});
-
-                        animate = true;
-                        $('#'+ socket.id).animate({ 'left': x, 'top': y }, () =>{
-                            animate = false;
-                        });
-                    }
-                };
-
-                $scope.newMessage = () => {
-                    let message = $scope.message;
-                    const messageData = {
-                        type: {
-                            code:1
-                        }, 
-                        username,
-                        text: message
-                    };  
-
-                    if( messageData.text && messageData.text.length > 0 && messageData.text.length < 100 ) {
-
-                        $scope.messages.push(messageData); 
-                        socket.emit('newMessage', messageData);
-                        showBubble(socket.id, message);
-                        scrollTop();
-
-                    }else{
-                        alert("Please type something in the message input \nor \nType less than 100 characters ");
-                    }
-                    $scope.message = null;
-                    
-                    
-
-                };
-            }).catch((err) => {
-                console.log(err);
-        });
-    };
+                }else{
+                    alert("Please type something in the message input \nor \nType less than 100 characters ");
+                }
+                $scope.message = null;
+            };
+        }catch(err){
+        console.log(err);
+    }
+    }
 }]);
